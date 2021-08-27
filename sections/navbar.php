4dbe32b0d21a67b49891006b879ad072bc838ecf
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-08-27 17:33:56 +0200

    Update

---
+++ sections/navbar.php
@@ -0,0 +1,14 @@
+<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
+<a class="navbar-brand" href="./index">
+    <img src=".\assets\img\logo\LTgold.png" width="50" height="30" class="d-inline-block align-top" alt="logo">LaninTata
+</a>
+  <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="navbar-nav">
+    <div class="nav-item text-nowrap">
+      <a class="nav-link px-3" href="#">Sign out</a>
+    </div>
+  </div>
+</header>
